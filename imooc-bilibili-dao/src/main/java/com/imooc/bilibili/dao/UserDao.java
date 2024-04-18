bfe3c1890a6bf1ef6052ba42208685251e9c1d76
RaymonOuO <54305858+RaymonOuO@users.noreply.github.com>
2024-04-18 17:07:31 -0400

    General features: register, login, update and jwt

---
+++ imooc-bilibili-dao/src/main/java/com/imooc/bilibili/dao/UserDao.java
@@ -0,0 +1,22 @@
+package com.imooc.bilibili.dao;
+
+import com.imooc.bilibili.domain.User;
+import com.imooc.bilibili.domain.UserInfo;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface UserDao {
+    User getUserByPhone(String phone);
+
+    Integer addUser(User user);
+
+    Integer addUserInfo(UserInfo userInfo);
+
+    User getUserById(Long id);
+
+    UserInfo getUserInfoByUserId(Long userId);
+
+    Integer updateUsers(User user);
+
+    User getUserByPhoneOrEmail(String phoneOrEmail);
+}
---
+++ imooc-bilibili-dao/src/main/java/com/imooc/bilibili/domain/constant/UserConstant.java
@@ -0,0 +1,10 @@
+package com.imooc.bilibili.domain.constant;
+
+public interface UserConstant {
+    String GENDER_MALE = "0";
+    String GENDER_FEMALE = "1";
+    String GENDER_UNKNOW = "2";
+    String DEFAULT_BIRTH = "2000-01-01";
+    String DEFAULT_NICK = "萌新";
+
+}
