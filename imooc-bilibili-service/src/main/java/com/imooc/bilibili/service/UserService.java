28440e234fcdac864cb9dbcaef022a4d6e605f80
RaymonOuO <54305858+RaymonOuO@users.noreply.github.com>
2024-04-22 22:47:38 -0400

    Auth integrated with AOP

--- imooc-bilibili-service/src/main/java/com/imooc/bilibili/service/UserService.java
+++ imooc-bilibili-service/src/main/java/com/imooc/bilibili/service/UserService.java
@@ -25,6 +25,9 @@ public class UserService {
     @Autowired
     private UserDao userDao;
 
+    @Autowired
+    private UserAuthService userAuthService;
+
     public void addUser(User user) {
         String phone = user.getPhone();
         if (StringUtils.isNullOrEmpty(phone)) {
@@ -62,6 +65,9 @@ public class UserService {
         userInfo.setCreateTime(now);
 
         userDao.addUserInfo(userInfo);
+
+        //添加用户默认权限角色
+        userAuthService.addUserDefaultRole(user.getId());
 
     }
 
