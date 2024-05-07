e3615051a9d7adea14a5a66f76d3326f2fb3d314
RaymonOuO <54305858+RaymonOuO@users.noreply.github.com>
2024-05-07 22:13:57 +0800

    Video features

--- imooc-bilibili-dao/src/main/java/com/imooc/bilibili/domain/constant/UserMomentsConstant.java
+++ imooc-bilibili-dao/src/main/java/com/imooc/bilibili/domain/constant/UserMomentsConstant.java
@@ -3,4 +3,6 @@ package com.imooc.bilibili.domain.constant;
 public interface UserMomentsConstant {
     String GROUP_MOMENTS = "MomentsGroup";
     String TOPIC_MOMENTS = "Topic-Moments";
+    String GROUP_DANMUS = "DanmusGroup";
+    String TOPIC_DANMUS = "Topic-Danmus";
 }
--- imooc-bilibili-service/src/main/java/com/imooc/bilibili/service/VideoService.java
+++ imooc-bilibili-service/src/main/java/com/imooc/bilibili/service/VideoService.java
@@ -2,6 +2,7 @@ package com.imooc.bilibili.service;
 
 import com.imooc.bilibili.dao.VideoDao;
 import com.imooc.bilibili.domain.*;
+import com.imooc.bilibili.domain.exception.ConditionException;
 import com.imooc.bilibili.service.util.FastDFSUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -10,6 +11,7 @@ import org.springframework.transaction.annotation.Transactional;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.*;
+import java.util.stream.Collectors;
 
 @Service
 public class VideoService {
@@ -18,6 +20,12 @@ public class VideoService {
 
     @Autowired
     private FastDFSUtil fastDFSUtil;
+
+    @Autowired
+    private UserCoinService userCoinService;
+
+    @Autowired
+    private UserService userService;
 
     @Transactional
     public void addVideos(Video video) {
@@ -60,4 +68,203 @@ public class VideoService {
             fastDFSUtil.viewVideoOnlineBySlices(request, response, url);
         }catch (Exception ignored){}
     }
+
+    public void addVideoLike(Long videoId, Long userId) {
+        Video video = videoDao.getVideoById(videoId);
+        if(video == null){
+            throw new ConditionException("非法视频！");
+        }
+        VideoLike videoLike = videoDao.getVideoLikeByVideoIdAndUserId(videoId, userId);
+        if(videoLike != null){
+            throw new ConditionException("已经赞过！");
+        }
+        videoLike = new VideoLike();
+        videoLike.setVideoId(videoId);
+        videoLike.setUserId(userId);
+        videoLike.setCreateTime(new Date());
+        videoDao.addVideoLike(videoLike);
+    }
+
+    public void deleteVideoLike(Long videoId, Long userId) {
+        videoDao.deleteVideoLike(videoId, userId);
+    }
+
+    public Map<String, Object> getVideoLikes(Long videoId, Long userId) {
+        Long count = videoDao.getVideoLikes(videoId);
+        VideoLike videoLike = videoDao.getVideoLikeByVideoIdAndUserId(videoId, userId);
+        boolean like = videoLike != null;
+        Map<String, Object> result = new HashMap<>();
+        result.put("count", count);
+        result.put("like", like);
+        return result;
+    }
+
+    @Transactional
+    public void addVideoCollection(VideoCollection videoCollection, Long userId) {
+        Long videoId = videoCollection.getVideoId();
+        Long groupId = videoCollection.getGroupId();
+        if(videoId == null || groupId == null){
+            throw new ConditionException("参数异常！");
+        }
+        Video video = videoDao.getVideoById(videoId);
+        if(video == null){
+            throw new ConditionException("非法视频！");
+        }
+        //删除原有视频收藏
+        videoDao.deleteVideoCollection(videoId, userId);
+        //添加新的视频收藏
+        videoCollection.setUserId(userId);
+        videoCollection.setCreateTime(new Date());
+        videoDao.addVideoCollection(videoCollection);
+    }
+
+    @Transactional
+    public void updateVideoCollection(VideoCollection videoCollection, Long userId) {
+        Long videoId = videoCollection.getVideoId();
+        Long groupId = videoCollection.getGroupId();
+        if(videoId == null || groupId == null){
+            throw new ConditionException("参数异常！");
+        }
+        Video video = videoDao.getVideoById(videoId);
+        if(video == null){
+            throw new ConditionException("非法视频！");
+        }
+        videoCollection.setUserId(userId);
+        videoDao.updateVideoCollection(videoCollection);
+    }
+
+    public void deleteVideoCollection(Long videoId, Long userId) {
+        videoDao.deleteVideoCollection(videoId, userId);
+    }
+
+    public Map<String, Object> getVideoCollections(Long videoId, Long userId) {
+        Long count = videoDao.getVideoCollections(videoId);
+        VideoCollection videoCollection = videoDao.getVideoCollectionByVideoIdAndUserId(videoId, userId);
+        boolean like = videoCollection != null;
+        Map<String, Object> result = new HashMap<>();
+        result.put("count", count);
+        result.put("like", like);
+        return result;
+    }
+
+    @Transactional
+    public void addVideoCoins(VideoCoin videoCoin, Long userId) {
+        Long videoId = videoCoin.getVideoId();
+        Integer amount = videoCoin.getAmount();
+        if(videoId == null){
+            throw new ConditionException("参数异常！");
+        }
+        Video video = videoDao.getVideoById(videoId);
+        if(video == null){
+            throw new ConditionException("非法视频！");
+        }
+        //查询当前登录用户是否拥有足够的硬币
+        Integer userCoinsAmount = userCoinService.getUserCoinsAmount(userId);
+        userCoinsAmount = userCoinsAmount == null ? 0 : userCoinsAmount;
+        if(amount > userCoinsAmount){
+            throw new ConditionException("硬币数量不足！");
+        }
+        //查询当前登录用户对该视频已经投了多少硬币
+        VideoCoin dbVideoCoin = videoDao.getVideoCoinByVideoIdAndUserId(videoId, userId);
+        //新增视频投币
+        if(dbVideoCoin == null){
+            videoCoin.setUserId(userId);
+            videoCoin.setCreateTime(new Date());
+            videoDao.addVideoCoin(videoCoin);
+        }else{
+            Integer dbAmount = dbVideoCoin.getAmount();
+            dbAmount += amount;
+            //更新视频投币
+            videoCoin.setUserId(userId);
+            videoCoin.setAmount(dbAmount);
+            videoCoin.setUpdateTime(new Date());
+            videoDao.updateVideoCoin(videoCoin);
+        }
+        //更新用户当前硬币总数
+        userCoinService.updateUserCoinsAmount(userId, (userCoinsAmount-amount));
+    }
+
+    public Map<String, Object> getVideoCoins(Long videoId, Long userId) {
+        Long count = videoDao.getVideoCoinsAmount(videoId);
+        VideoCoin videoCollection = videoDao.getVideoCoinByVideoIdAndUserId(videoId, userId);
+        boolean like = videoCollection != null;
+        Map<String, Object> result = new HashMap<>();
+        result.put("count", count);
+        result.put("like", like);
+        return result;
+    }
+
+    public void addVideoComment(VideoComment videoComment, Long userId) {
+        Long videoId = videoComment.getVideoId();
+        if(videoId == null){
+            throw new ConditionException("参数异常！");
+        }
+        Video video = videoDao.getVideoById(videoId);
+        if(video == null){
+            throw new ConditionException("非法视频！");
+        }
+        videoComment.setUserId(userId);
+        videoComment.setCreateTime(new Date());
+        videoDao.addVideoComment(videoComment);
+    }
+
+    public PageResult<VideoComment> pageListVideoComments(Integer size, Integer no, Long videoId) {
+        Video video = videoDao.getVideoById(videoId);
+        if(video == null){
+            throw new ConditionException("非法视频！");
+        }
+        Map<String, Object> params = new HashMap<>();
+        params.put("start", (no-1)*size);
+        params.put("limit", size);
+        params.put("videoId", videoId);
+        Integer total = videoDao.pageCountVideoComments(params);
+        List<VideoComment> list = new ArrayList<>();
+        if(total > 0){
+            list = videoDao.pageListVideoComments(params);
+            if(!list.isEmpty()){
+                //批量查询二级评论
+                List<Long> parentIdList = list.stream().map(VideoComment::getId).collect(Collectors.toList());
+                //批量查询用户信息
+                Set<Long> userIdList = list.stream().map(VideoComment::getUserId).collect(Collectors.toSet());
+                List<VideoComment> childCommentList = videoDao.batchGetVideoCommentsByRootIds(parentIdList);
+                Set<Long> replyUserIdList = childCommentList.stream()
+                        .map(VideoComment::getUserId).collect(Collectors.toSet());
+                Set<Long> childUserIdList = childCommentList.stream()
+                        .map(VideoComment::getReplyUserId).collect(Collectors.toSet());
+                userIdList.addAll(replyUserIdList);
+                userIdList.addAll(childUserIdList);
+                List<UserInfo> userInfoList = userService.batchGetUserInfoByUserIds(userIdList);
+                Map<Long, UserInfo> userInfoMap = userInfoList.stream()
+                        .collect(Collectors.toMap(UserInfo :: getUserId, userInfo -> userInfo));
+                list.forEach(comment -> {
+                    Long id = comment.getId();
+                    List<VideoComment> childList = new ArrayList<>();
+                    childCommentList.forEach(child -> {
+                        if(id.equals(child.getRootId())){
+                            child.setUserInfo(userInfoMap.get(child.getUserId()));
+                            child.setReplyUserInfo(userInfoMap.get(child.getReplyUserId()));
+                            childList.add(child);
+                        }
+                    });
+                    comment.setChildList(childList);
+                    comment.setUserInfo(userInfoMap.get(comment.getUserId()));
+                });
+            }
+        }
+        return new PageResult<>(total, list);
+    }
+
+    public Map<String, Object> getVideoDetails(Long videoId) {
+        Video video =  videoDao.getVideoDetails(videoId);
+        Long userId = video.getUserId();
+        User user = userService.getUserInfo(userId);
+        UserInfo userInfo = user.getUserInfo();
+        Map<String, Object> result = new HashMap<>();
+        result.put("video", video);
+        result.put("userInfo", userInfo);
+        return result;
+    }
+
+
+
 }
